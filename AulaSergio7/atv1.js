0d4c00410d8cf4634dbe182a57888f66aca55d06
Gabriela Silva <163335365+gabipsilvaofc@users.noreply.github.com>
2024-05-15 17:15:07 -0300

    Aula sobre funções

---
+++ AulaSergio7/atv1.js
@@ -0,0 +1,27 @@
+
+let num1 = Number(prompt("Digite um numero:")),
+num2 = Number(prompt("Digite um numero:"))
+
+function calculaSub(subtracao){
+    subtracao = num1 - num2
+    alert(subtracao)
+}
+calculaSub()
+
+function calculaAdi(adicao){
+    adicao = num1 + num2
+    alert(adicao)
+}
+calculaAdi()
+
+function calculaMult(multiplicacao){
+    multiplicacao = num1 * num2
+    alert(multiplicacao)
+}
+calculaMult()
+
+function calculaDiv(divisao){
+    divisao = num1 / num2
+    alert(divisao)
+}
+calculaDiv()
